8cf07a2a65f69b8842ec52fd883c6f8f3fb4065d
Daniel Gonzalez <danielhumgon@gmail.com>
2020-06-20 20:57:27 -0400

    style(warning): Added open alpha warning notice

---
+++ src/components/version-status.js
@@ -0,0 +1,25 @@
+import React from "react"
+
+
+class VersionStatus extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+
+  }
+
+  render() {
+    return (
+      <>
+        <div className="version-status">
+            <div>
+                <p><b>Warning: Open Alpha - things will break</b></p>
+            </div>
+        </div>
+      </>
+    )
+  }
+
+}
+
+export default VersionStatus
