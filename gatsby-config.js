cf994857068e4e260f34696f331d4c0747e03f8a
Chris Troutner <chris.troutner@gmail.com>
2020-07-07 12:23:33 -0700

    fix(normal build): Trying to get normal build working

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,36 +1,36 @@
 // const ipfsPrefix = process.argv.find(val => val === '--prefix-paths')
 // console.log('DEBUG >> IM HERE')
 
-const ipfsConfig = [
-  'gatsby-plugin-ipfs',
-  'gatsby-plugin-react-helmet',
-  {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      start_url: '__GATSBY_IPFS_PATH_PREFIX__',
-      name: 'images',
-      path: `${__dirname}/src/images`
-    }
-  },
-  'gatsby-transformer-sharp',
-  'gatsby-plugin-sharp',
-  {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      name: 'gatsby-starter-default',
-      short_name: 'starter',
-      start_url: '__GATSBY_IPFS_PATH_PREFIX__',
-      background_color: '#663399',
-      theme_color: '#663399',
-      display: 'minimal-ui',
-      icon: 'src/images/gatsby-icon.png' // This path is relative to the root of the site.
-    }
-  }
-
-  // this (optional) plugin enables Progressive Web App + Offline functionality
-  // To learn more, visit: https://gatsby.dev/offline
-  // `gatsby-plugin-offline`,
-]
+// const ipfsConfig = [
+//   'gatsby-plugin-ipfs',
+//   'gatsby-plugin-react-helmet',
+//   {
+//     resolve: 'gatsby-source-filesystem',
+//     options: {
+//       start_url: '__GATSBY_IPFS_PATH_PREFIX__',
+//       name: 'images',
+//       path: `${__dirname}/src/images`
+//     }
+//   },
+//   'gatsby-transformer-sharp',
+//   'gatsby-plugin-sharp',
+//   {
+//     resolve: 'gatsby-plugin-manifest',
+//     options: {
+//       name: 'gatsby-starter-default',
+//       short_name: 'starter',
+//       start_url: '__GATSBY_IPFS_PATH_PREFIX__',
+//       background_color: '#663399',
+//       theme_color: '#663399',
+//       display: 'minimal-ui',
+//       icon: 'src/images/gatsby-icon.png' // This path is relative to the root of the site.
+//     }
+//   }
+//
+//   // this (optional) plugin enables Progressive Web App + Offline functionality
+//   // To learn more, visit: https://gatsby.dev/offline
+//   // `gatsby-plugin-offline`,
+// ]
 
 const normalConfig = [
   'gatsby-plugin-react-helmet',
