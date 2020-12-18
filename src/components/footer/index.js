4646b1cb7ca6b3571d67a47e39a61470021ce5ab
Chris Troutner <chris.troutner@gmail.com>
2020-12-18 14:38:59 -0800

    linting

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -57,61 +57,61 @@ class Footer extends React.Component {
 
   render () {
     return (
-      <section id="footer">
-        <Row className="footer-container">
-          <Col md={5} className="footer-section">
+      <section id='footer'>
+        <Row className='footer-container'>
+          <Col md={5} className='footer-section'>
             <Row>
-              <Col md={12} className="mb-1">
-                <p className="section-tittle">Produced By</p>
+              <Col md={12} className='mb-1'>
+                <p className='section-tittle'>Produced By</p>
 
                 <a
-                  href="https://psfoundation.cash/"
-                  target="_blank"
-                  rel="noopener noreferrer"
+                  href='https://psfoundation.cash/'
+                  target='_blank'
+                  rel='noopener noreferrer'
                 >
                   Permissionless Software Foundation
                 </a>
               </Col>
               <Col md={12}>
-                <p className="section-tittle">Source Code</p>
-                <FontAwesomeIcon className="" size="lg" icon={faGithub} />
+                <p className='section-tittle'>Source Code</p>
+                <FontAwesomeIcon className='' size='lg' icon={faGithub} />
                 <a
-                  href="https://github.com/Permissionless-Software-Foundation/gatsby-ipfs-web-wallet"
-                  target="_blank"
-                  rel="noopener noreferrer"
+                  href='https://github.com/Permissionless-Software-Foundation/gatsby-ipfs-web-wallet'
+                  target='_blank'
+                  rel='noopener noreferrer'
                 >
                   Github
                 </a>
               </Col>
             </Row>
           </Col>
-          <Col md={7} className="footer-section">
-            <div className="pull-right">
-              <span className="section-tittle">
+          <Col md={7} className='footer-section'>
+            <div className='pull-right'>
+              <span className='section-tittle'>
                 Ways to access this web-app
               </span>
               <ul>
-                <li id="web">
+                <li id='web'>
                   <span>
                     <b>Web</b>
                   </span>
                   <b>|</b>
-                  <a href="https://wallet.fullstack.cash">
+                  <a href='https://wallet.fullstack.cash'>
                     wallet.fullstack.cash
                   </a>
                 </li>
 
-                <li id="tor">
+                <li id='tor'>
                   <span>
                     <b>Tor</b>
                   </span>
                   <b>|</b>
-                  <a href="http://2egutot63q765ciwsenlcy5zdyxwxt7olzbldr5dx5i3ixsef2nvrzid.onion/">
+                  <a href='http://2egutot63q765ciwsenlcy5zdyxwxt7olzbldr5dx5i3ixsef2nvrzid.onion/'>
                     2egutot63q765ciwsenlcy5zdyxwxt7olzbldr5dx5i3ixsef2nvrzid.onion
                   </a>
                 </li>
 
-                <li id="ipfs">
+                <li id='ipfs'>
                   <span>
                     <b>IPFS</b>
                   </span>
