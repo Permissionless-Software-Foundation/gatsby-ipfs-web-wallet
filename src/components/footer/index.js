4ea0e4a0d0faecdcbfe604deb7ccf4f6b4522d69
Chris Troutner <chris.troutner@gmail.com>
2020-12-18 15:12:50 -0800

    Updating hash checkpoint

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -35,7 +35,7 @@ class Footer extends React.Component {
   async handleMemoService () {
     // This is a hard-coded hash or 'checkpoint' to use in times when the
     // connection fails.
-    const hash = 'QmXT85Xoi7xMRD9m7Ta4Cx8Yrsd2WSzLrq2VRo26KW4xLu'
+    const hash = 'QmXhhzNZ3dcKEC1JE428zVVVfXuzqUAzpWdKf6wdfHPtEi'
 
     // Try to retrieve the hash from the BCH blockchain.
     try {
