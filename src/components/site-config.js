73d7623774eb5b81a1b8b9842bebc277b0f22ae2
Chris Troutner <chris.troutner@gmail.com>
2020-12-18 16:04:46 -0800

    fix(footer): Adding footer info to the site-config.js file

--- src/components/site-config.js
+++ src/components/site-config.js
@@ -11,7 +11,14 @@ const config = {
 
   // The BCH address used in a memo.cash account. Used for tracking the IPFS
   // hash of the mirror of this site.
-  memoAddr: 'bitcoincash:qqwdv3hkmvd5vk0uhwqrqnef54542e5ctvy3ppt0nq'
+  memoAddr: 'bitcoincash:qqwdv3hkmvd5vk0uhwqrqnef54542e5ctvy3ppt0nq',
+
+  // Footer Information
+  hostText: 'FullStack.cash',
+  hostUrl: 'https://fullstack.cash/',
+  sourceCode: 'https://github.com/Permissionless-Software-Foundation/gatsby-ipfs-web-wallet',
+  torUrl: '2egutot63q765ciwsenlcy5zdyxwxt7olzbldr5dx5i3ixsef2nvrzid.onion',
+  clearWebUrl: 'https://gatsby-ipfs-web-wallet.fullstack.cash'
 }
 
 module.exports = config
